970e24adbf643cd965c4e7eb371710b87ee1d5a9
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-01-06 04:24:39 +0600

    cors frontend url import

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://helplink-frontend.vercel.app"],
     credentials: true
 }));
 app.use(express.json());
