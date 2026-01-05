efbd6cb99d624563581252901b2a053ad82a4093
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-01-06 05:16:46 +0600

    cors update

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,8 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://helplink-frontend.vercel.app"],
+    // origin: ["http://localhost:3000", "https://helplink-frontend.vercel.app"],
+    origin: "https://helplink-frontend.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true
 }));
