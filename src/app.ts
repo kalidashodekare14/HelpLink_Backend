d0be867b94355ea4954d496dcc582a71f8071915
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-01-06 04:40:46 +0600

    Cors options add

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ app.use(cors({
     origin: ["http://localhost:3000", "https://helplink-frontend.vercel.app"],
     credentials: true
 }));
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
